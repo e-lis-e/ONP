00d7b83b0a2ad0a8a961064c4ce41f1ac9d477dc
elise <Elisenovaes10@gmail.com>
2024-10-03 15:38:19 -0300

    Add entity

---
+++ src/main/java/br/com/politics/ONP/entities/Postagem.java
@@ -0,0 +1,27 @@
+package br.com.politics.ONP.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+//provisório
+public class Postagem {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String usuario;
+    private String titulo;
+    private String conteudo;
+    private Boolean curtida;
+    
+}
