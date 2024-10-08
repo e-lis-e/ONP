c1024083778db3ff51df96b0a325582fc329f8e3
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-10-07 21:26:24 -0300

    criação de packages

---
+++ src/main/java/br/com/politics/ONP/services/cometario/ComentarioService.java
@@ -0,0 +1,4 @@
+package br.com.politics.ONP.services.cometario;
+
+public interface ComentarioService {
+}
---
+++ src/main/java/br/com/politics/ONP/services/discussao/DiscussaoService.java
@@ -0,0 +1,4 @@
+package br.com.politics.ONP.services.discussao;
+
+public interface DiscussaoService {
+}
