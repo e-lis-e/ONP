26ae1bcdc3fdbb265d2c325c005cc5dfedcd1f84
elise <Elisenovaes10@gmail.com>
2024-10-11 15:02:04 -0300

    Update PostagemService.java

--- src/main/java/br/com/politics/ONP/services/postagem/PostagemService.java
+++ src/main/java/br/com/politics/ONP/services/postagem/PostagemService.java
@@ -1,4 +1,45 @@
 package br.com.politics.ONP.services.postagem;
 
-public interface PostagemService {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.politics.ONP.entities.Postagem;
+import br.com.politics.ONP.entities.Usuario;
+import br.com.politics.ONP.repositories.PostagemRepository;
+
+@Service
+public class PostagemService {
+
+    @Autowired
+    private PostagemRepository postagemRepository;
+
+    public List<Postagem> findAll() {
+        return postagemRepository.findAll();
+    }
+
+    public Optional<Postagem> findById(Long postagem_id) {
+        return postagemRepository.findById(postagem_id);
+    }
+
+    public List<Postagem> findByUsuario(Long usuario_id) {
+        return postagemRepository.findByUsuario(usuario_id);
+    }
+
+    public Postagem save(Postagem postagem) {
+        return postagemRepository.save(postagem);
+    }
+
+    public Postagem update(Long postagem_id, Postagem postagemDetails) {
+        return postagemRepository.findById(postagem_id).map(postagem -> {
+            postagem.setTitulo(postagemDetails.getTitulo());
+            postagem.setConteudo(postagemDetails.getConteudo());
+            return postagemRepository.save(postagem);
+        }).orElseThrow(() -> new RuntimeException("Postagem não encontrada com o ID: " + postagem_id));
+    }
+
+    public void delete(Long postagem_id) {
+        postagemRepository.deleteById(postagem_id);
+    }
 }
