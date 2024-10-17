f8c50096e89bba487dde7ab691e49e46b9738c4f
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-10-17 13:38:28 -0300

    Create, get and delete usuário

--- src/main/java/br/com/politics/ONP/services/usuario/UsuarioService.java
+++ src/main/java/br/com/politics/ONP/services/usuario/UsuarioService.java
@@ -1,16 +1,17 @@
 package br.com.politics.ONP.services.usuario;
 
 import br.com.politics.ONP.entities.Usuario;
+import br.com.politics.ONP.exceptions.UsuarioExistenteException;
 
 import java.util.List;
 
 public interface UsuarioService {
 
-    public Usuario cadastrarUsuario(Usuario usuario);
+    public Usuario cadastrarUsuario(Usuario usuario) throws UsuarioExistenteException;
     public Usuario atualizarUsuario(Usuario usuario);
-    public void removerUsuario(Usuario usuario);
+    public void removerUsuario(Long id);
     public List<Usuario> listarUsuarios();
     public Usuario buscarUsuarioPorEmail(String email);
-    public Usuario buscarUsuarioPorId(Integer id);
+    public Usuario buscarUsuarioPorId(Long id);
     public Usuario buscarUsuarioPorLoginESenha(String login, String senha);
 }
--- src/main/java/br/com/politics/ONP/services/usuario/UsuarioServiceImpl.java
+++ src/main/java/br/com/politics/ONP/services/usuario/UsuarioServiceImpl.java
@@ -1,12 +1,14 @@
 package br.com.politics.ONP.services.usuario;
 
 import br.com.politics.ONP.entities.Usuario;
+import br.com.politics.ONP.exceptions.UsuarioExistenteException;
 import br.com.politics.ONP.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService{
@@ -16,8 +18,14 @@ public class UsuarioServiceImpl implements UsuarioService{
 
     @Override
     @Transactional
-    public Usuario cadastrarUsuario(Usuario usuario) {
-        return null;
+    public Usuario cadastrarUsuario(Usuario usuario) throws UsuarioExistenteException {
+
+        Optional<Usuario> usuariobanco = usuarioRepository.findByEmail(usuario.getEmail());
+        if (usuariobanco.isPresent()) {
+            throw new UsuarioExistenteException();
+        }
+
+        return usuarioRepository.save(usuario);
     }
 
     @Override
@@ -26,23 +34,24 @@ public class UsuarioServiceImpl implements UsuarioService{
     }
 
     @Override
-    public void removerUsuario(Usuario usuario) {
-
+    public void removerUsuario(Long id) {
+        Optional<Usuario> usuariobanco = usuarioRepository.findById(id);
+        usuarioRepository.delete(usuariobanco.get());
     }
 
     @Override
     public List<Usuario> listarUsuarios() {
-        return List.of();
+        return usuarioRepository.findAll();
     }
 
     @Override
     public Usuario buscarUsuarioPorEmail(String email) {
-        return null;
+        return usuarioRepository.findByEmail(email).get();
     }
 
     @Override
-    public Usuario buscarUsuarioPorId(Integer id) {
-        return null;
+    public Usuario buscarUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id).get();
     }
 
     @Override
