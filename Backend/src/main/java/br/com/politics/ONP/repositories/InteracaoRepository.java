d3887dfb3d6e77cb0c0c5dabdc29e52ed9e0ac25
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-11-14 14:05:12 -0300

    interacao repository

--- Backend/src/main/java/br/com/politics/ONP/repositories/InteracaoRepository.java
+++ Backend/src/main/java/br/com/politics/ONP/repositories/InteracaoRepository.java
@@ -3,5 +3,10 @@ package br.com.politics.ONP.repositories;
 import br.com.politics.ONP.entities.Interacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InteracaoRepository extends JpaRepository<Interacao, Long> {
+    Optional<Interacao> findByUsuarioId(Long usuario_id);
+    Optional<Interacao> findByComentarioId(Long comentario_id);
+    Optional<Interacao> findByPostagemId(Long postagem_id);
 }
