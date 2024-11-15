d91abbae544fe877ce940ee7d24c5195a0b497b1
elise <Elisenovaes10@gmail.com>
2024-11-14 22:38:32 -0300

    Adição de rotas no App.tsx

--- frontend/ONP/src/App.tsx
+++ frontend/ONP/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import LoginScreen from './Pages/Login/LoginScreen';
 import './App.css'
+import RegisterScreen from './Pages/Register/RegisterScreen';
+import Perfil from './Pages/Perfil/Perfil';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/Dashboard" element={<Dashboard/>} />
+                <Route path='/Login' element={<LoginScreen/>}/>
+                <Route path='/Register' element={<RegisterScreen/>}/>
+                <Route path='/Profile' element={<Perfil/>}/>
+            </Routes>
+        </Router>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+};
 
 export default App
