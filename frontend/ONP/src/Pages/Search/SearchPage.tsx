322c18c49dfeb97993c76511946e0ba262ce97df
elise <Elisenovaes10@gmail.com>
2024-11-20 22:37:08 -0300

    Create SearchPage.tsx

---
+++ frontend/ONP/src/Pages/Search/SearchPage.tsx
@@ -0,0 +1,19 @@
+//import React from "react";
+import './Dashboard.css';
+import Header from '../../Components/Header/Header';
+
+const SearchPage = () => {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="searchContent">
+    
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default SearchPage
