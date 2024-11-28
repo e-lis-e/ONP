01c1c2fa5a71b254675c47f33179ea81752deddc
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-11-28 14:52:11 -0300

    Update UsuarioExistenteException.java

--- Backend/src/main/java/br/com/politics/ONP/exceptions/usuario/UsuarioExistenteException.java
+++ Backend/src/main/java/br/com/politics/ONP/exceptions/usuario/UsuarioExistenteException.java
@@ -4,8 +4,4 @@ public class UsuarioExistenteException extends RuntimeException{
     public UsuarioExistenteException() {
         super("Um outro usuário com este e-mail já existe.");
     }
-
-    public UsuarioExistenteException(String message, Throwable cause){
-        super(message, cause);
-    }
 }
