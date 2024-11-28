45915d2411baa03604615c9a8c2268453b3f6284
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-11-28 14:13:09 -0300

    Update UsuarioNaoEncontradoException.java

--- Backend/src/main/java/br/com/politics/ONP/exceptions/usuario/UsuarioNaoEncontradoException.java
+++ Backend/src/main/java/br/com/politics/ONP/exceptions/usuario/UsuarioNaoEncontradoException.java
@@ -6,12 +6,6 @@ public class UsuarioNaoEncontradoException extends RuntimeException {
         super("Usuário com e-mail " + message + " não encontrado.");
     }
 
-    public UsuarioNaoEncontradoException(Throwable cause) {
-        super("Usuário não encotrado.", cause);
-    }
-    public UsuarioNaoEncontradoException(String message, Throwable cause) {
-        super(message, cause);
-    }
     public UsuarioNaoEncontradoException() {
         super("ID inexistente");
     }
