86f19500bf16d972ef1ed5979be3c97101723439
elise <Elisenovaes10@gmail.com>
2024-12-03 19:39:57 -0300

    adição de css em Login e Register

--- frontend/ONP/src/Pages/Login/LoginScreen.tsx
+++ frontend/ONP/src/Pages/Login/LoginScreen.tsx
@@ -1,14 +1,20 @@
-//import React from "react";
+import { Link } from 'react-router-dom';
+import '../LoginRegister.css';
 
 const LoginScreen = () => {
     return (
-        <div className="container">
-            <h2>Acesse seu perfil no DCyber</h2>
-            <form className="loginForm">
-                <input type="text" placeholder="E-mail" required/>
-                <input type="password" placeholder="Senha" required/>
-                <button type="submit">Entrar</button>
-            </form>
+        <div className="page">
+            <div className="containerUser">
+                <h2>Acesse seu perfil no DCyber</h2>
+                <form>
+                    <input type="email" placeholder="E-mail" required />
+                    <input type="password" placeholder="Senha" required />
+                    <button type="submit" 
+                    //onClick={handleLogin}
+                    >Entrar</button>
+                </form>
+                <Link to="/Register" className='link'>Novo usuário?</Link>
+            </div>
         </div>
     );
 };
--- frontend/ONP/src/Pages/Register/RegisterScreen.tsx
+++ frontend/ONP/src/Pages/Register/RegisterScreen.tsx
@@ -1,15 +1,21 @@
-//import React from "react";
+import { Link } from 'react-router-dom';
+import '../LoginRegister.css';
 
 const RegisterScreen = () => {
     return (
-        <div className="container">
-            <h2>Cadastre-se</h2>
-            <form className="registerForm">
-                <input type="text" placeholder="Nome de Usuário" required/>
-                <input type="text" placeholder="E-mail" required/>
-                <input type="password" placeholder="Senha" required/>
-                <button type="submit">Cadastrar Perfil</button>
-            </form>
+        <div className="page">
+            <div className="containerUser">
+                <h2>Cadastre-se</h2>
+                <form>
+                    <input type="text" placeholder="Nome de Usuário" required />
+                    <input type="email" placeholder="E-mail" required />
+                    <input type="password" placeholder="Senha" required />
+                    <button type="submit" 
+                    //onClick={handleRegister}
+                    >Cadastrar Perfil</button>
+                </form>
+                <Link to="/Login" className='link'>Já possui conta?</Link>
+            </div>
         </div>
     );
 };
