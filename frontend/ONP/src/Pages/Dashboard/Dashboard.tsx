084b51c86e8534bae47fe990700adc978a0d7fa1
elise <Elisenovaes10@gmail.com>
2024-12-03 19:39:15 -0300

    Atualização de dashboard
    lógica e aparência

--- frontend/ONP/src/Pages/Dashboard/Dashboard.tsx
+++ frontend/ONP/src/Pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,48 @@
 //import React from "react";
 import './Dashboard.css';
 import Header from '../../Components/Header/Header';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
+    // const [discussoes, setDiscussoes] = useState([]);
+    const [postagens, setPostagens] = useState([]);
+
+    useEffect(() => {
+        
+        fetch('/api/postagens')
+        .then((res) => res.json())
+        .then((data: InstanceType<typeof postagens>[]) => setPostagens(data))
+        .catch((error) => console.error('Erro ao buscar postagens:', error));
+    }, []);
+
     return (
-        <div>
+        <div className='dashboard'>
+            <div className="container">
             <Header />
-            <div className="mainBlock">
                 <div className="dashboardContent">
                     <div className="discussoes">
                         <h2>Discussões</h2>
+                         {postagens.length > 0 ? (
+                            <ul>
+                                {postagens.map((discussao) => (
+                                    <li key={post.postagem_id}>{post.titulo}</li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>Nenhuma discussão encontrada</p>
+                        )}
                     </div>
                     <div className="posts">
                         <h2>Postagens</h2>
+                        {postagens.length > 0 ? (
+                            <ul>
+                                {postagens.map((post) => (
+                                    <li key={post.postagem_id}>{post.titulo} - <span>{post.usuario?.nome}</span></li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>Nenhuma postagem encontrada</p>
+                        )}
                     </div>
                 </div>
             </div>
