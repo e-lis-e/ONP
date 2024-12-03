ab46a0df05c904cf88307b3a22f69ff31470701c
elise <Elisenovaes10@gmail.com>
2024-12-03 20:49:07 -0300

    Correções e adições no header

--- frontend/ONP/src/Components/Header/Header.tsx
+++ frontend/ONP/src/Components/Header/Header.tsx
@@ -1,20 +1,48 @@
-//import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaPlus, FaSearch, FaUserCircle } from 'react-icons/fa';
 import './header.css'
+import ProfileModal from '../ProfileModal/ProfileModal';
+import PostagemModal from '../PostagemModal/PostagemModal';
 
 const Header = () => {
+    const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
+    const [isPostagemModalOpen, setIsPostagemModalOpen] = useState(false);
     const navigate = useNavigate();
 
-    const handleProfile = () => {
-        navigate('/Perfil');
-    };
+    const handlePostagemModal = () => {
+        setIsPostagemModalOpen((prev) => !prev);
+    }
+
+    const handleProfileModal = () => {
+        setIsProfileModalOpen((prev) => !prev);
+    }
+
+    const handleSearch = () => {
+        navigate('/Search')
+    }
+
+    const handleDashboard = () => {
+        navigate('/')
+    }
 
     return (
         <div className="header">
-            <h1 className='title'>DCyber</h1>
-            <input type='text' placeholder='Pesquisar postagens e discussões' className='searchbar'></input>
-            <FaUserCircle onClick={handleProfile} className='profilePic' size={30} color='#007bff'/>
+            <h1 className='title' onClick={handleDashboard}>DCyber</h1>
+            <div className="search-container">
+                <input type='text' placeholder=' Pesquisar postagens e discussões' className='searchbar'></input>
+                <FaSearch onClick={handleSearch} />
+            </div>
+            <div className="postDiv">
+                <FaPlus onClick={handlePostagemModal} className='postOpen' size={30} color='333'/>
+                <PostagemModal isOpen={isPostagemModalOpen} onClose={handlePostagemModal}/>
+            </div>
+            
+            <div className="profileDiv">
+                <FaUserCircle onClick={handleProfileModal} className='profilePic' size={30} color='#333'/>
+                <ProfileModal isOpen={isProfileModalOpen} onClose={handleProfileModal} />
+            </div>
+          
         </div>
     )
 }
