59b1670f28557782109224c0f06325009b23f05d
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-12-05 14:23:08 -0300

    autenticação e segurança

---
+++ Backend/src/main/java/br/com/politics/ONP/services/auth/AuthServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.politics.ONP.services.auth;
+
+
+import br.com.politics.ONP.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username).get();
+    }
+}
