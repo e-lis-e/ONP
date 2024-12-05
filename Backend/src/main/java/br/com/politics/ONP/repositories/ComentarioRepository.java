f6ffc258544c2d73a345759be31aa3d87f851f31
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-12-05 12:31:29 -0300

    Update ComentarioRepository.java

--- Backend/src/main/java/br/com/politics/ONP/repositories/ComentarioRepository.java
+++ Backend/src/main/java/br/com/politics/ONP/repositories/ComentarioRepository.java
@@ -1,10 +1,17 @@
 package br.com.politics.ONP.repositories;
 
+import br.com.politics.ONP.entities.Interacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.politics.ONP.entities.Comentario;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
+    public List<Comentario> findByUsuarioId(Long usuario_id);
+    public List<Comentario> findByPostagemId(Long postagem_id);
+    public List<Comentario> findByComentarioPaiId(Long comentario_id);
     
 }
