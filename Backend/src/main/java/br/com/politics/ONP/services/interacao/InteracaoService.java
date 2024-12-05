910532785254056fde4b01ad00c413dbcdf0f3d7
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-12-05 13:01:03 -0300

    Update InteracaoService.java

--- Backend/src/main/java/br/com/politics/ONP/services/interacao/InteracaoService.java
+++ Backend/src/main/java/br/com/politics/ONP/services/interacao/InteracaoService.java
@@ -1,4 +1,15 @@
 package br.com.politics.ONP.services.interacao;
 
+import br.com.politics.ONP.entities.Interacao;
+
+import java.util.List;
+
 public interface InteracaoService {
+
+    public void interagir(Interacao interacao);
+    public void alterarInteracao(Interacao interacao);
+    public void excluirInteracao(Interacao interacao);
+    public List<Interacao> buscarInteracoesDaPostagem(Long postagem_id);
+    public List<Interacao> buscarInteracoesDoComentario(Long postagem_id);
+    public List<Interacao> buscarInteracoesDoUsusario(Long usuario_id);
 }
